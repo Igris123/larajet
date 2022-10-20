614ea390dfbc16ad5e054a7aa55096c74721e1e9
Igris123 <a.rivaslpez@uandresbello.edu>
2022-10-20 03:58:05 -0300

    cambios landing

---
+++ resources/views/landingpage.blade.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Faad Express</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link href="/css/app.css" rel="stylesheet">
+    
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container">
+          <a class="navbar-brand" href="#"><span class="text-warning">Faad</span>Express</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">              
+              <li class="nav-item">
+                <a class="nav-link" href="#">Inicio</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#cotizador">Cotizador</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#Seguimiento">Seguimiento</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#servicios">Servicios</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#nosotros">Nosotros</a>
+              </li>
+            
+            </ul>
+           
+          </div>
+        </div>
+      </nav>
+      
+
+
+      <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="false">
+        <div class="carousel-indicators">
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="/img/autocarga.jpg" class="d-block w-100" alt="...">
+            <div class="carousel-caption d-none d-md-block">
+              <h5>Seguridad</h5>
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui modi, atque animi ullam quia maiores quo rerum libero incidunt ipsum corporis ex illo unde quasi distinctio quibusdam, consequuntur repudiandae dignissimos?</p>
+            </div>
+            
+          </div>
+          <div class="carousel-item">
+            <img src="/img/repartidor.jpg" class="d-block w-100" alt="...">
+            <div class="carousel-caption d-none d-md-block">
+              <h5>Confiabilidad</h5>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus inventore earum nulla fugiat magnam quos dolorum? Aliquid animi officiis, distinctio, necessitatibus delectus, veniam sed nam fugiat repellat ullam voluptates reprehenderit.</p>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img src="/img/auto3.jpg" class="d-block w-100" alt="...">
+            <div class="carousel-caption d-none d-md-block">
+              <h5>Rapidez</h5>
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos voluptatum animi velit obcaecati magni? Velit sed sunt blanditiis magnam doloremque vel ducimus, iste necessitatibus perspiciatis eius amet nisi magni ipsum?</p>
+            </div>
+          </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Anterior</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Siguiente</span>
+        </button>
+      </div>
+
+    <script src = "https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
+    
+</body>
+</html>
+
+
+    
